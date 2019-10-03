6dc811502c1fa86c171aadc12403f4bd21bac5e5
volkovAlexey <alv-volkov@ukr.net>
2019-10-03 23:29:55 +0300

    add ShopProjectMain

---
+++ src/main/java/ShopProjectMain.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProjectMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopProjectMain.class, args);
+    }
+}
