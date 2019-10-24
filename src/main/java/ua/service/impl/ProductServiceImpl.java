8cd8d22353af4395d2611b2345176098582a74f8
volkovAlexey <alv-volkov@ukr.net>
2019-10-24 09:02:08 +0300

    fix ProductService

--- src/main/java/ua/service/impl/ProductServiceImpl.java
+++ src/main/java/ua/service/impl/ProductServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-//@Transactional
+@Transactional
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
